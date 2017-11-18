651491dc8579d35c5e465d9120e5491de76df372
Vishnu R Menon <>
2017-11-18 11:16:55 +0000

    missing equal sign WTFFFFFF NEEEED COFFFEEE PLESS agggginninasd

--- AdminUI/www/js/user.js
+++ AdminUI/www/js/user.js
@@ -129,7 +129,7 @@ $(document).ready(function() {
       $.get({url: 'getData.php'}, function(data, status) {
         data = JSON.parse(data)
           console.log(data[0]["Description"])
-        if (data[0]["Description"] = "FALL" && flag == false) {
+        if (data[0]["Description"] == "FALL" && flag == false) {
           $('.wrapper').prepend('<div class="alert alert-danger"><div class="container-fluid"><div class="alert-icon"><i class="material-icons">error_outline</i></div><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true"><i class="material-icons">clear</i></span></button><b>Error Alert:</b> A fall was detected by the fall detector. Please send help.</div></div>')
         }
         flag = true;
