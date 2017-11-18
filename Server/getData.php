c3326709988f34406e2a88c11160f3aef744b9f2
Beniamin BIA <bbia@clj.bitdefender.biz>
2017-11-18 09:17:54 +0000

    design implemented

--- Server/getData.php
+++ Server/getData.php
@@ -1,14 +1,6 @@
 <?php
 require 'database.php';
 
-header('Access-Control-Allow-Methods: GET, POST'); 
-header("Access-Control-Allow-Headers: X-Requested-With"); 
-header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
-header('Access-Control-Allow-Methods: GET, POST');
-header("Access-Control-Allow-Headers: X-Requested-With");
 
 $conn = openConnection();
 $entries = readAlerts($conn);
