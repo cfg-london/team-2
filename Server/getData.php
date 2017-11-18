3ec3f116152810b029709754123906547b6ef1ec
Vishnu R Menon <>
2017-11-18 08:31:10 +0000

    updated getData method with cors headers

--- Server/getData.php
+++ Server/getData.php
@@ -3,6 +3,7 @@ require 'database.php';
 
 $conn = openConnection();
 $entries = readAlerts($conn);
-
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
 echo json_encode($entries);
 ?>
